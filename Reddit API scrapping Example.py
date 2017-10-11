5d402918b2a54a9561d2d77d37be5e21163314b2
Ussinova Aliya <aliya@MacBook-Pro-de-Aliya.local>
2017-10-11 19:40:54 +0200

    json script with top reddit posts

--- Reddit API scrapping Example.py
+++ Reddit API scrapping Example.py
@@ -1,9 +1,20 @@
 # import the libraries for API scrapping 
 import requests 
 import requests.auth
-from authorization_details import token
-from authorization_details import secret 
+from authorization_details import * 
 
 client_auth = requests.auth.HTTPBasicAuth(token, secret)
-print(token)
-print(secret)
+post_data = {"grant_type": "password", "username":username, "password":password}
+headers ={'User-agent':'Formation API'}
+response = requests.post("https://www.reddit.com/api/v1/access_token", auth = client_auth, data = post_data, 
+                        headers = headers)
+print(response.json()["access_token"])
+
+authorization = "bearer " + response.json()["access_token"]
+print(authorization)
+headers = {"authorization":authorization, "User-agent": "Formation API"}
+params = {"t":"week"}
+# Get top reddit posts on topic python for the last week
+response = requests.get("https://oauth.reddit.com/r/python/top", headers = headers, params = params)
+python_top = response.json()
+print(python_top)
