c32021cc4094c1631029d3470a43779f100045c4
Ussinova Aliya <aliya@MacBook-Pro-de-Aliya.local>
2017-10-14 18:30:36 +0200

    Saved into dataframe

--- weathersite_scrapping.py
+++ weathersite_scrapping.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 page = requests.get("http://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168#.WeIXlUqCzq0")
 content = page.content
@@ -25,4 +26,11 @@ period_tags = forecast[0].select(".tombstone-container .period-name")
 periods = [pt.get_text() for pt in period_tags]
 short_descr = [desc.get_text() for desc in forecast[0].select(".tombstone-container .short-desc")]
 description = [d['title'] for d in forecast[0].select(".tombstone-container img")]
-print(description)
+temp = [t.get_text() for t in forecast[0].select(".tombstone-container .temp")]
+
+data = pd.DataFrame({"period":periods,
+                    "description": description,
+                    "short_description": short_descr,
+                    "temperature": temp})
+
+print(data)
